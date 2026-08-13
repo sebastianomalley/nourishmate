e84e8e5a7e9649a2fb35e0ba47065dfbd12f6497
Jason O'Malley <60304720+sebastianomalley@users.noreply.github.com>
2026-08-13 16:56:34 -0400

    StaticFiles_Storage

--- nourishmate/nourishmate/nourishmate/settings.py
+++ nourishmate/nourishmate/nourishmate/settings.py
@@ -128,6 +128,8 @@ STATIC_URL = 'static/'
 
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
